56bd623855c51b4cbae25080b3a79513ac9e62af
Nanda <fernandaalvesmonteiro7@gmail.com>
2015-12-03 09:30:16 -0200

    Adicionando Interfaces

---
+++ Interfaces/MediaAritmetica.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.interfaces;
+
+/**
+ *
+ * @author NandaPC
+ */
+public interface MediaAritmetica {
+ 
+    public float calculaMedia(float nota1, float nota2, float nota3);
+}
